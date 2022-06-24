046637ae60e98e8cc0d8c84c6d0b6fb770fdb8cd
isaacng <isaacng@apppay.tech>
2022-06-24 17:35:05 +0800

    First commit of the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experiments:{
+    topLevelAwait: true,
+  }
 }
 
-module.exports = nextConfig
+module.exports = {
+  // target: 'experimental-serverless-trace',
+  webpack: (config) => {
+    config.experiments = config.experiments || {};
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+};
